a76f8d74ac69ed348a9a0ab8c6f3b8de520d7844
João <159589732+Sapoo0@users.noreply.github.com>
2025-10-06 13:36:21 -0300

    Calculadora do Menos

---
+++ CalculadoraDeVezes.java
@@ -0,0 +1,24 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class CalculadoraDeVezes {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// Painel inicial
+		System.out.println("=== CALCULADORA DO * === ");
+
+		// Entrada de Dados
+		System.out.println("Digite um número: ");
+		int valorA = sc.nextInt();
+
+		System.out.println("Digite outro número: ");
+		int valorB = sc.nextInt();
+
+		// Saida de Dados
+		System.out.printf("%d * %d = %d%n", valorA, valorB, valorA * valorB);
+
+		sc.close();
+	}
+}
